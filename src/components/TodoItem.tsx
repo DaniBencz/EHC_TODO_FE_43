53bd71c0e5cb15e9cdd7573915032e301b501738
DaniBencz <daniel.bencz@gmail.com>
2025-09-22 10:25:43 +0200

    fix long text edge-case

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,6 @@
 import type { ITodoItemProps, TCheckItem } from "../types";
 
-const ItemCheck = ({ uncheckItem, id }: { uncheckItem: TCheckItem, id: number }) => (
+const ItemCheck = ({ uncheckItem, id }: { uncheckItem: TCheckItem, id: number; }) => (
   <div className="w-5 h-5 rounded-full bg-gradient-to-br from-blue-400 to-purple-500 flex items-center justify-center"
     onClick={() => uncheckItem(id)}>
     <img src="/icon-check.svg" alt="Check" className="w-3 h-3" />
@@ -9,16 +9,16 @@ const ItemCheck = ({ uncheckItem, id }: { uncheckItem: TCheckItem, id: number })
 
 const TodoItem = ({ item, checkItem, uncheckItem, deleteItem }: ITodoItemProps) => {
   return (
-    <div className="p-4 flex items-center gap-4 group hover:bg-gray-750 relative" key={item.id}>
+    <div className="p-4 flex gap-4 group hover:bg-gray-750 relative" key={item.id}>
       <div className="hover:cursor-pointer">
 
         {item.status === 'done' ? <ItemCheck uncheckItem={uncheckItem} id={item.id} /> :
           <div className="w-5 h-5 rounded-full border-2 border-gray-500"
             onClick={() => checkItem(item.id)}></div>}
       </div>
-      <span className={`flex-1 ${item.status === 'done' ? 'text-gray-500 line-through' : 'text-gray-300'}`}>
+      <div className={`flex-1 min-w-0 break-words pr-8 ${item.status === 'done' ? 'text-gray-500 line-through' : 'text-gray-300'}`}>
         {item.title}
-      </span>
+      </div>
       <img
         src="/icon-cross.svg"
         alt="Delete"
